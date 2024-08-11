395706c9daa5812f744cfc67d7aa4792aca6891c
Gavin <gavin@gavdev.xyz>
2024-08-11 16:48:52 -0400

    Add sops flake

---
+++ sops.nix
@@ -0,0 +1,26 @@
+
+
+{
+  description = "NixOS configuration with configurable hostname and system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+
+  inputs.defaultConfig = {
+    hostname = "nixos";
+    system = "x86_64-linux";
+  };
+
+  outputs = { self, nixpkgs, sops-nix, ... }@inputs: {
+    nixosConfigurations = {
+      ${inputs.defaultConfig.hostname} = nixpkgs.lib.nixosSystem {
+        system = inputs.defaultConfig.system;
+        modules = [
+          sops-nix.nixosModules.sops
+        ];
+      };
+    };
+  };
+}
