2a9bdfa2fac755d2815fde56ec854f3c066d0086
Gavin Daly <gav.d@me.com>
2024-08-15 18:03:09 -0400

    feat(ssh): enable OpenSSH daemon with security configurations

---
+++ modules/ssh.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      # AllowUsers = [ "" ];
+    };
+  };
+}
