59ce5232e780371e98e0d05728d6b1f45c3dc9cc
Gavin Daly <gav.d@me.com>
2024-08-21 02:31:29 -0400

    build: Update defaultPackage to use nixpkgs reference

--- flake.nix
+++ flake.nix
@@ -1,41 +1,15 @@
 {
-  description = "First Base Config with Kubernetes k3s Cluster Support";
+  description = "First Base Config";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-    unstable.url = "nixpkgs/nixos-unstable";
+    # unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Add the nixpkgs-fmt input
-    nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, unstable, nixpkgs-fmt, ... }:
-    let
-      # Define a mapping of architectures to NixOS system identifiers
-      architectures = {
-        aarch64 = "aarch64-linux";
-        x86_64 = "x86_64-linux";
-        armv7l = "armv7l-linux"; # Example for 32-bit ARM
-        # Add more architectures as needed
-      };
+  outputs = { self, nixpkgs, home-manager, ... }:
 
-      # Function to generate system configuration for a given architecture and role
-      makeSystem = arch: role: nixpkgs.lib.nixosSystem {
-        system = architectures.${arch};
-        modules = [
-          {
-            imports = [ ./rpi-${role}.nix ];
-
-            # Set the hostname using the architecture and role
-            networking.hostName = "k3s-${arch}-${role}";
-
-            # Other system-specific or role-specific configurations
-          }
-        ];
-      };
-    in
     {
       nixosConfigurations = {
         # Existing systems
@@ -54,7 +28,6 @@
               # arguments to home.nix
             }
           ];
-          specialArgs = { inherit unstable; };
         };
 
         hestia = nixpkgs.lib.nixosSystem {
@@ -64,39 +37,6 @@
           ];
         };
 
-        ares = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/ares.nix
-          ];
-        };
-
-        hypnos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/hypnos.nix
-          ];
-        };
-
-        # k3s nodes for Raspberry Pi at the bottom
-        rpi-master = makeSystem "aarch64" "master";
-        rpi-worker01 = makeSystem "aarch64" "worker01";
-        rpi-worker02 = makeSystem "aarch64" "worker02";
-        rpi-worker03 = makeSystem "aarch64" "worker03";
-
-        # Add additional architectures and roles as needed
-        # rpi-armv7l-master = makeSystem "armv7l" "master";
-        # rpi-armv7l-worker01 = makeSystem "armv7l" "worker01";
       };
-
-      # Add formatter to the devShell
-      devShell = nixpkgs.mkShell {
-        nativeBuildInputs = [
-          nixpkgs-fmt
-        ];
-      };
-
-      # Add a defaultPackage attribute that does nothing
-      defaultPackage.x86_64-linux = null;
     };
 }
