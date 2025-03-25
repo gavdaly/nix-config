f20d7ad6bc9670b23a78630dfb867ddfc14f3e43
Gavin Daly <gav.d@me.com>
2025-03-25 11:04:21 -0400

    It is working now

--- hosts/athena.nix
+++ hosts/athena.nix
@@ -1,106 +1,30 @@
 { pkgs, ... }:
 
 {
-  # User-specific settings for Gavin
+    # Basic system packages
+    environment.systemPackages = with pkgs; [
+      bat
+      ripgrep
+      btop 
+      devenv
+      zoxide
+      direnv
+    ];
 
-
-    # Define the programs you want to manage with Home Manager
-    # programs.zsh.enable = true;
-    # programs.zsh.loginShell = true;
-
-    # programs.git = {
-    #   enable = true;
-    #   userName = "Gavin";          # Replace with your Git username
-    #   userEmail = "gavin@gavdev.xyz";  # Replace with your Git email
-
-    #   extraConfig = {
-    #     # Aliases
-    #     "alias.co" = "checkout";
-    #     "alias.br" = "branch";
-    #     "alias.ci" = "commit";
-    #     "alias.st" = "status";
-    #     "alias.last" = "log -1 HEAD";
-        
-    #     # Set default branch name to 'main'
-    #     "init.defaultBranch" = "main";
-
-    #     # Push default configuration
-    #     "push.default" = "simple";
-
-    #     # Set the default editor to Neovim
-    #     "core.editor" = "nvim";
-        
-    #     # Color configuration
-    #     "color.ui" = "auto";
-        
-    #     # Credential caching
-    #     "credential.helper" = "osxkeychain";
-
-    #     # Enable rebasing by default for pull
-    #     "pull.rebase" = "true";
-    #   };
-    # };
-
-    # Installing `eza` as a replacement for `ls`, with colors, icons, and git status
-    # programs.eza = {
-    #   enable = true;
-    #   alias = "ls";  # Optionally alias `eza` to `ls`
-    #   options = [
-    #     "--color=always"
-    #     "--icons"
-    #     "--git"  # Enable git status information in file listings
-    #   ];
-    # };
-
-    # Adding a custom alias for `eza` with `-la` flags
-    # programs.zsh.aliases = {
-    #   l = "eza -la --color=always --icons --git";
-    # };
-
-    # Recommended additional programs
-    # programs.bat.enable = true;
-    # programs.ripgrep.enable = true;
-    # programs.ytop.enable = true;
-    # programs.fd.enable = true;
-    # programs.tealdeer.enable = true;
-    # programs.delta.enable = true;
-
-    # Installing and configuring `zellij`
-    # programs.zellij.enable = true;
-
-    # Installing and configuring `devenv`
+    # The following programs.* configurations won't work without Home Manager
     # programs.devenv.enable = true;
-
-    # Installing `zoxide`
     # programs.zoxide.enable = true;
-
-    # Installing and configuring nix-direnv
     # programs.direnv = {
     #   enable = true;
     #   nix-direnv.enable = true;
-    #   # extraInit = ''
-    #   #   use nix
-    #   # '';
-    # };
-
-    # Installing command-line utilities including GitHub CLI
-    # pkgs = with pkgs; [
-    #   nixpkgs-fmt
-    #   jq
-    #   curl
-    #   wget
-    #   gh
-    #   bat
-    # ];
-
-    # Set up neovim as your editor
-    # programs.neovim = {
-    #   enable = true;
-    #   viAlias = true;
-    #   vimAlias = true;
-    #   extraConfig = ''
-    #     set number
-    #     syntax on
+    #   extraInit = ''
+    #     use nix
     #   '';
     # };
+
+    # If you're using zsh, you might want this instead
+    environment.interactiveShellInit = ''
+      eval "$(direnv hook zsh)"
+      eval "$(zoxide init zsh)"
+    '';
 }
--- mac/flake.nix
+++ mac/flake.nix
@@ -2,15 +2,13 @@
   description = "macOS Config for MacBook Pro (Athena)";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable"; # Changed to unstable
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
   };
 
-  outputs = inputs@{ nixpkgs, darwin, home-manager, ... }:
+  outputs = inputs@{ nixpkgs, darwin, ... }:
     let
       system = "aarch64-darwin";
       pkgs = import nixpkgs { inherit system; };
@@ -19,16 +17,11 @@
       darwinConfigurations = {
         athena = darwin.lib.darwinSystem {
           system = system;
-
+          
           modules = [
-            # ../hosts/athena.nix
-            home-manager.darwinModules.home-manager
+            ../hosts/athena.nix
             {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # home-manager.users.gavin = import ../hosts/athena.nix;
-              home-manager.users.gavin.home.stateVersion = "24.05";
-
+              # Removed home-manager configuration
               services.nix-daemon.enable = true; # Enable the Nix daemon
               nix.useDaemon = true; # Ensure daemon mode is used
             }
